0ffdba59103ee0dd21cf78fab2f1c85e8e6f03c6
Jessica Petty <jepetty@microsoft.com>
2018-07-26 10:19:05 -0700

    Update RadixSort.java

--- Sorts/src/sort/RadixSort.java
+++ Sorts/src/sort/RadixSort.java
@@ -8,7 +8,7 @@ class RadixSort {
     private static int getMax(int arr[], int n) {
         int mx = arr[0];
         for (int i = 1; i < n; i++)
-            if (arr[i] > mx + 1)
+            if (arr[i] > mx)
                 mx = arr[i];
         return mx;
     }
