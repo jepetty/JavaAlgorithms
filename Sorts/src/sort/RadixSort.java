977db210b9df252ded01b4a9a2d27ca8c28b9e9b
Jessica Petty <jepetty@microsoft.com>
2018-07-26 10:20:19 -0700

    Fix off by one error

--- Sorts/src/sort/RadixSort.java
+++ Sorts/src/sort/RadixSort.java
@@ -8,7 +8,7 @@ class RadixSort {
     private static int getMax(int arr[], int n) {
         int mx = arr[0];
         for (int i = 1; i < n; i++)
-            if (arr[i] > mx)
+            if (arr[i] > mx + 1)
                 mx = arr[i];
         return mx;
     }
